94101b157e4961325ec8d0a3d1cde7fc3d20e50c
vktrbr <viktorbarbarich@gmail.com>
2022-03-10 19:25:16 +0300

    function parser + streamlit func parser | v0.1

--- opml/function_parser/sympy_parser.py
+++ opml/function_parser/sympy_parser.py
@@ -1,26 +1,29 @@
 from __future__ import annotations
-import sympy as sp
+import sympy
 from sympy.parsing.sympy_parser import parse_expr
 from typing import AnyStr
-
-# assert isinstance(function, sp.core.expr.Expr), 'Function is not sympy'
-# assert len(function.free_symbols) <= 1, 'Function depend on more than 1 variables'
-# if len(function.free_symbols) == 0:
-#     print('Function independent on variables. code 0')
-#     middle_point = (a + b) / 2
-#     return {'point': middle_point, 'f_value': float(function)}, history
-#
-# function = sp.lambdify(list(function.free_symbols), function, 'numpy')
+import re
 
 
 class Text2Sympy:
 
     @staticmethod
-    def parse_func(function_string: AnyStr) -> sp.core.expr.Expr:
+    def parse_func(function_string: AnyStr) -> sympy.core.expr.Expr:
         """
         Convert the string to sympy.core.expr.Expr
         :param function_string: a string with function that is written by python rules
         :return: function as sympy Expression
         """
+        log_pattern = re.compile(r'log.+\(.+\)')
+        logarithms = re.findall(log_pattern, function_string)
+        if len(logarithms) > 0:
+            for log in logarithms:
+                log: AnyStr
+                log_base = log[3:log.find('(')]
+                log_arg = log[log.find('(') + 1:log.find(')')]
+                log_right = f'log({log_arg}, {log_base})'
+                function_string = function_string.replace(log, log_right)
+
         function_sympy = parse_expr(function_string)
+        function_sympy = function_sympy.subs({sympy.symbols('e'): sympy.exp(1)})
         return function_sympy
--- opml/streamlit_app.py
+++ opml/streamlit_app.py
@@ -1,17 +1,32 @@
-# import sympy
-# import streamlit as st
-#
-# from function_parser.sympy_parser import Text2Sympy
+"""
+For local running streamlit app : streamlit run opml/streamlit_app.py
+"""
+from typing import AnyStr
 
-# st.write('# Hello world')
-# function = st.text_input('Enter the function here', 'x**2 + x + 1')
-# function = Text2Sympy.parse_func(function)
-# function = function.subs({sympy.symbols('e'): sympy.exp(1)})
-# st.write('> $$ f(x) = ', rf'\ {sympy.latex(function)}$$')
-# value = st.text_input('Enter the point here', '2')
-# value = Text2Sympy.parse_func(value)
-# st.write('> $$ f(x) = ', rf'\ {sympy.latex(function.subs({list(function.free_symbols)[0]: value}))}$$')
+import sympy
+import streamlit as st
+from function_parser.sympy_parser import Text2Sympy
+import re
 
+st.set_page_config(
+    page_title="OneD optimization",
+    page_icon=":balloon:",
+)
 
-# if __name__ == '__main__':
-#     print(Text2Sympy)
+algorithms_list = ["Golden-section search", "Successive parabolic interpolation",
+                   "Brent's method", "Broyden–Fletcher–Goldfarb–Shanno algorithm"]
+
+st.title('1-D Optimization')
+
+st.sidebar.markdown('## Task conditions:')
+function = st.sidebar.text_input('Enter the function here', 'log3(x) + e ** (x + pi) + 1 / x + x ** 2', )
+
+if re.sub(r'\s', '', function) != '':
+
+    try:
+        function_sympy = Text2Sympy.parse_func(function)
+        st.sidebar.write(r'> $ \displaystyle f(x) = ', rf'\ {sympy.latex(sympy.sympify(function))} $')
+        flag_empty_func = False
+    except (SyntaxError, TypeError):
+        st.sidebar.write('Check syntax. Wrong input :(')
+
