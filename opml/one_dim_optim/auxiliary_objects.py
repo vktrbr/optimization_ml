8e0d901c08fabb0383375f92c5ffcff4cdfd97f7
Victor Barbarich <52676181+vktrbr@users.noreply.github.com>
2022-03-11 23:45:40 +0300

    typing problem with version

--- opml/one_dim_optim/auxiliary_objects.py
+++ opml/one_dim_optim/auxiliary_objects.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
 
-from typing import List, TypedDict
+from typing import List
+import sys
 from numbers import Real
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict 
+else:
+    from typing_extensions import TypedDict
+
 
 
 class History(TypedDict):
