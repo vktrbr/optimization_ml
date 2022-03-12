9199095cc88467639b80daa91d0b81f9d7f89e44
vktrbr <viktorbarbarich@gmail.com>
2022-03-13 01:29:11 +0300

    added init files

--- opml/__init__.py
+++ opml/__init__.py
@@ -5,5 +5,6 @@ import function_parser
 import one_dim_optim
 import plot_funcs
 
+
 __all__ = [sympy_parser, test_sympy_parser, auxiliary_objects, golden_section_search, simple_plot,
            function_parser, one_dim_optim, plot_funcs]
