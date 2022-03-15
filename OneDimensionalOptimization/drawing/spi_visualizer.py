a329dc348bfc3c911ae6791985438399b0b571b1
vktrbr <viktorbarbarich@gmail.com>
2022-03-15 13:02:01 +0300

    Added Decimal for more precision calculations. Added colors to points and curves

--- OneDimensionalOptimization/drawing/spi_visualizer.py
+++ OneDimensionalOptimization/drawing/spi_visualizer.py
@@ -7,6 +7,7 @@ import plotly.graph_objects as go
 from OneDimensionalOptimization.algorithms.support import HistorySPI
 from typing import Callable, Tuple, Any
 from numbers import Real
+from decimal import Decimal
 
 
 def gen_animation_spi(func: Callable[[Real, Any], Real],
@@ -20,7 +21,7 @@ def gen_animation_spi(func: Callable[[Real, Any], Real],
     :param bounds: tuple with left and right points on the x-axis
     :param func: the functions for which the story was created
     """
-    print(history)
+
     n = len(history['iteration'])
     x_axis = np.linspace(bounds[0], bounds[1], 200)
     f_axis = np.zeros_like(x_axis)
@@ -33,14 +34,22 @@ def gen_animation_spi(func: Callable[[Real, Any], Real],
     f_range = [min(f_axis) - diff_f * 0.1, max(f_axis) + diff_f * 0.1]
 
     history = pd.DataFrame(history)
-
-    a, b, c = make_parabolic_function(history.loc[0, 'x_left'],
-                                      history.loc[0, 'x_middle'],
-                                      history.loc[0, 'x_right'],
+    a, b, c = make_parabolic_function(history.loc[0, 'x0'],
+                                      history.loc[0, 'x1'],
+                                      history.loc[0, 'x2'],
                                       func, **kwargs)
 
-    data = [go.Scatter(x=x_axis, y=a * x_axis ** 2 + b * x_axis + c, name='parabola'),
-            go.Scatter(x=x_axis, y=f_axis, name='function')]
+    x0, x1, x2 = history.loc[0, ['x0', 'x1', 'x2']].values
+
+    data = [go.Scatter(x=x_axis, y=float(a) * x_axis ** 2 + float(b) * x_axis + float(c),
+                       name='parabola 1', marker={'color': 'rgba(55, 101, 164, 1)'}),
+            go.Scatter(x=[float(x0)], y=[func(float(x0), **kwargs)],
+                       name='x0', mode='markers', marker={'size': 10, 'color': 'rgba(66, 122, 161, 1)'}),
+            go.Scatter(x=[float(x1)], y=[func(float(x1), **kwargs)],
+                       name='x1', mode='markers', marker={'size': 10, 'color': 'rgba(66, 122, 161, 1)'}),
+            go.Scatter(x=[float(x2)], y=[func(float(x2), **kwargs)],
+                       name='x2', mode='markers', marker={'size': 10, 'color': 'rgba(231, 29, 54, 1)'}),
+            go.Scatter(x=x_axis, y=f_axis, name='function', marker={'color': 'rgba(0, 0, 0, 0.8)'})]
 
     layout = go.Layout({'font': {'size': 14},
                         # 'legend': {'itemsizing': 'constant', 'title': {'text': 'type'}, 'tracegroupgap': 0},
@@ -48,10 +57,11 @@ def gen_animation_spi(func: Callable[[Real, Any], Real],
                                      'currentvalue': {'prefix': 'iteration='},
                                      'len': 0.9,
                                      'pad': {'b': 10, 't': 60},
-                                     'steps': [{'args': [[f'{i}'], {'frame': {'duration': 300, 'redraw': True},
+                                     'steps': [{'args': [[f'{i}'], {'frame': {'duration': 500, 'redraw': False,
+                                                                              'mode': 'immediate'},
                                                                     'mode': 'immediate',
-                                                                    'fromcurrent': True,
-                                                                    'transition': {'duration': 300,
+                                                                    'fromcurrent': False,
+                                                                    'transition': {'duration': 200,
                                                                                    'easing': 'cubic-in-out'}}],
                                                 'label': f'{i}',
                                                 'method': 'animate'} for i in range(n)],
@@ -67,7 +77,7 @@ def gen_animation_spi(func: Callable[[Real, Any], Real],
                                                                       'mode': 'immediate',
                                                                       'fromcurrent': True,
                                                                       'transition':
-                                                                          {'duration': 500, 'easing': 'cubic-in-out'}}],
+                                                                          {'duration': 100, 'easing': 'cubic-in-out'}}],
                                                       'label': '&#9654;',
                                                       'method': 'animate'},
                                                      {'args': [[None], {'frame': {'duration': 0,
@@ -80,7 +90,7 @@ def gen_animation_spi(func: Callable[[Real, Any], Real],
                                                       'method': 'animate'}],
                                          'direction': 'left',
                                          'pad': {'r': 10, 't': 70},
-                                         'showactive': False,
+                                         'showactive': True,
                                          'type': 'buttons',
                                          'x': 0.1,
                                          'xanchor': 'right',
@@ -89,28 +99,38 @@ def gen_animation_spi(func: Callable[[Real, Any], Real],
                         'xaxis': {'anchor': 'y',
                                   'domain': [0.0, 1.0],
                                   'range': x_range,
+                                  'autorange': False,
                                   'title': {'text': '<b>x</b>, c.u.'}},
                         'yaxis': {'anchor': 'x',
                                   'domain': [0.0, 1.0],
                                   'range': f_range,
+                                  'autorange': False,
                                   'title': {'text': '<b>f(x)</b>, c.u.'}}
                         })
 
     frames = []
     for i in range(1, history.shape[0]):
 
-        x0, x1, x2 = history.loc[i, ['x_left', 'x_middle', 'x_right']].values
+        x0, x1, x2 = history.loc[i, ['x0', 'x1', 'x2']].values
         a, b, c = make_parabolic_function(x0, x1, x2, func, **kwargs)
-        parabola = a * x_axis ** 2 + b * x_axis + c
+
+        parabola = float(a) * x_axis ** 2 + float(b) * x_axis + float(c)
         frames.append(go.Frame({
-               'data': [go.Scatter(x=x_axis, y=parabola, name='parabola'),
-                        go.Scatter(x=[x0], y=[func(x0, **kwargs)], name='left', mode='markers'),
-                        go.Scatter(x=[x1], y=[func(x1, **kwargs)], name='middle', mode='markers'),
-                        go.Scatter(x=[x2], y=[func(x2, **kwargs)], name='right', mode='markers')],
+               'data': [go.Scatter(x=x_axis, y=parabola, name=f'parabola {i + 1}',
+                                   marker={'color': 'rgba(55, 101, 164, 1)'}),
+                        go.Scatter(x=[float(x0)], y=[func(float(x0), **kwargs)],
+                                   name='x0', mode='markers', marker={'size': 10, 'color': 'rgba(66, 122, 161, 1)'}),
+                        go.Scatter(x=[float(x1)], y=[func(float(x1), **kwargs)],
+                                   name='x1', mode='markers', marker={'size': 10, 'color': 'rgba(66, 122, 161, 1)'}),
+                        go.Scatter(x=[float(x2)], y=[func(float(x2), **kwargs)],
+                                   name='x2', mode='markers', marker={'size': 10, 'color': 'rgba(231, 29, 54, 1)'})],
 
                'name': f'{i}'}))
 
     fig = go.Figure(data=data, layout=layout, frames=frames)
+    fig.update_xaxes(range=x_range)
+    fig.update_yaxes(range=f_range)
+
     return fig
 
 
@@ -127,9 +147,10 @@ def make_parabolic_function(x0: Tuple[Real, Real],
     :param func: the functions for which the story was created
     :return: Coefficients of the parabolic function
     """
-    f0 = func(x0, **kwargs)
-    f1 = func(x1, **kwargs)
-    f2 = func(x2, **kwargs)
+    x0, x1, x2 = Decimal(x0), Decimal(x1), Decimal(x2)
+    f0 = Decimal(func(float(x0), **kwargs))
+    f1 = Decimal(func(float(x1), **kwargs))
+    f2 = Decimal(func(float(x2), **kwargs))
     a = (f0 * x1 - f0 * x2 - f1 * x0 + f1 * x2 + f2 * x0 - f2 * x1) / (
                 x0 ** 2 * x1 - x0 ** 2 * x2 - x0 * x1 ** 2 + x0 * x2 ** 2 + x1 ** 2 * x2 - x1 * x2 ** 2)
     b = (-f0 * x1 ** 2 + f0 * x2 ** 2 + f1 * x0 ** 2 - f1 * x2 ** 2 - f2 * x0 ** 2 + f2 * x1 ** 2) / (
