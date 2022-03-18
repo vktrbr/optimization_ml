e65cd318d4187b88c4fa839b60f75d2d24b56724
vktrbr <viktorbarbarich@gmail.com>
2022-03-19 02:30:09 +0300

    Brant algorithm done

--- OneDimensionalOptimization/algorithms/brant.py
+++ OneDimensionalOptimization/algorithms/brant.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-from typing import Tuple, Callable, Any, Literal
+from typing import Tuple, Callable, Any, Literal, Sequence
 from OneDimensionalOptimization.algorithms.support import *
-import numpy as np
 
 
 def brant(function: Callable[[Real, Any], Real],
@@ -14,7 +13,8 @@ def brant(function: Callable[[Real, Any], Real],
           keep_history: bool = False,
           **kwargs) -> Tuple[Point, HistoryGSS]:
     """
-    Brant's algorithm
+    Brant's algorithm.
+    Brent, R. P., Algorithms for Minimization Without Derivatives. Englewood Cliffs, NJ: Prentice-Hall, 1973 pp.72-80
 
     :param function: callable that depends on the first positional argument. Other arguments are passed through kwargs
     :param bounds: tuple with two numbers. This is left and right bound optimization. [a, b]
@@ -23,108 +23,167 @@ def brant(function: Callable[[Real, Any], Real],
     :param max_iter: maximum number of iterations
     :param verbose: flag of printing iteration logs
     :param keep_history: flag of return history
+
+    :var gold_const: b - (b - a) / phi = a + (b - a) * gold_const
+    :var type_opt_const: This value unifies the optimization for each type of min and max
     :return: tuple with point and history.
 
     """
 
     type_optimization = type_optimization.lower().strip()
     assert type_optimization in ['min', 'max'], 'Invalid type optimization. Enter "min" or "max"'
-    
+
     if type_optimization == 'max':
         type_opt_const = -1
     else:
         type_opt_const = 1
-        
+
+    gold_const = (3 - 5 ** 0.5) / 2
+    remainder = 0.0  # p / q when we calculate x_new
+
+    # initial values
     a, b = sorted(bounds)
-    const_from_golden = 1 - 2 / (1 + 5 ** 0.5)
-    x = w = v = (a + b) / 2
-    fw = fv = fx = type_opt_const * function(x, **kwargs)
+    x_largest = x_middle = x_least = a + gold_const * (b - a)
+    f_largest = f_middle = f_least = type_opt_const * function(x_least, **kwargs)
 
-    deltax = 0.0
-    rat = b - a
+    history = {'iteration': [], 'f_least': [], 'f_middle': [], 'f_largest': [],  'x_least': [], 'x_middle': [],
+               'x_largest': [], 'left_bound': [], 'right_bound': [], 'type_step': []}
 
-    for i in range(max_iter):
-        tol1 = epsilon * np.abs(x) + 1.e-10
-        tol2 = 2.0 * tol1
-        x_middle = (a + b) / 2
-        # check for convergence
+    if keep_history:
+        history = update_history(history, [0, f_least, f_middle, f_largest,
+                                           x_least, x_middle, x_largest, a, b, 'initial'])
+    if verbose:
+        print(f'iteration 0\tx = {x_least:0.6f},\tf(x) = {f_least:0.6f}\ttype : initial')
 
-        if np.abs(x - x_middle) < (tol2 - 0.5 * (b - a)):
-            return x, type_opt_const * fx, i
+    for i in range(1, max_iter + 1):
+        middle_point = (a + b) / 2
+        tolerance = epsilon * abs(x_least) + 1e-9  # f is never evaluated at two points closer together than tolerance
 
-        if np.abs(deltax) <= tol1:
-            if x >= x_middle:
-                deltax = a - x  # do a golden section step
-            else:
-                deltax = b - x
+        # Check stopping criterion
+        if abs(x_least - middle_point) > 2 * tolerance - (b - a) / 2:
+            p = q = previous_remainder = 0
+            if abs(remainder) > tolerance:
+                
+                # fit parabola
+                p = ((x_least - x_largest) ** 2 * (f_least - f_middle) -
+                     (x_least - x_middle) ** 2 * (f_least - f_largest))
 
-            rat = const_from_golden * deltax
-        else:  # do a parabolic step
-            tmp1 = (x - w) * (fx - fv)
-            tmp2 = (x - v) * (fx - fw)
-            p = (x - v) * tmp2 - (x - w) * tmp1
-            tmp2 = 2.0 * (tmp2 - tmp1)
-            if tmp2 > 0.0:
-                p = -p
-            tmp2 = np.abs(tmp2)
-            dx_temp = deltax
-            deltax = rat
-            # check parabolic fit
-            if ((p > tmp2 * (a - x)) and (p < tmp2 * (b - x)) and
-                    (np.abs(p) < np.abs(0.5 * tmp2 * dx_temp))):
-                rat = p * 1.0 / tmp2  # if parabolic step is useful.
-                u = x + rat
-                if (u - a) < tol2 or (b - u) < tol2:
-                    if x_middle - x >= 0:
-                        rat = tol1
-                    else:
-                        rat = -tol1
-            else:
-                if x >= x_middle:
-                    deltax = a - x  # if it's not do a golden section step
+                q = 2 * ((x_least - x_largest) * (f_least - f_middle) -
+                         (x_least - x_middle) * (f_least - f_largest))
+
+                # change q sign to positive
+                if q > 0:
+                    p = -p
                 else:
-                    deltax = b - x
-                rat = const_from_golden * deltax
+                    q = -q
+                # r stores the previous value of remainder
+                previous_remainder = remainder
 
-        if np.abs(rat) < tol1:  # update by at least tol1
-            if rat >= 0:
-                u = x + tol1
-            else:
-                u = x - tol1
-        else:
-            u = x + rat
-        fu = type_opt_const * function(u, **kwargs)  # calculate new output value
+            # Check conditions for parabolic step:
+            # tol - x_new must not be close to x_least, so we check the step
+            # previous_remainder - the value of p / q at the second-last cycle
+            # |previous_remainder| > tol - is checked above
+            # q != 0 - includes in next conditions
+            # x_least + p / q in (a, b). New point in interval
+            # p / q < previous(p / q) / 2. Control the divergence
 
-        if fu > fx:  # if it's bigger than current
-            if u < x:
-                a = u
+            if abs(p) < 0.5 * abs(q * previous_remainder) and a * q < x_least * q + p < b * q:
+                remainder = p / q
+                x_new = x_least + remainder
+                name_step = 'parabolic'
+
+                # Check that f not be evaluated too close to a or b
+                if x_new - a < 2 * tolerance or b - x_new < 2 * tolerance:
+                    if x_least < middle_point:
+                        remainder = tolerance
+                    else:
+                        remainder = -tolerance
+
+            # If conditions above is false we do golden section step
             else:
-                b = u
-            if (fu <= fw) or (w == x):
-                v = w
-                w = u
-                fv = fw
-                fw = fu
-            elif (fu <= fv) or (v == x) or (v == w):
-                v = u
-                fv = fu
+                name_step = 'golden'
+                if x_least < middle_point:
+                    remainder = (b - x_least) * gold_const
+                else:
+                    remainder = (a - x_least) * gold_const
+
+            # Check that f not be evaluated too close to x_least
+            if abs(remainder) > tolerance:
+                x_new = x_least + remainder
+            elif remainder > 0:
+                x_new = x_least + tolerance
+            else:
+                x_new = x_least - tolerance
+
+            f_new = type_opt_const * function(x_new, **kwargs)
+
+            # Update a, b, x_largest, x_middle, x_leas
+            if f_new <= f_least:
+                if x_new < x_least:
+                    b = x_least
+                else:
+                    a = x_least
+
+                x_largest = x_middle
+                f_largest = f_middle
+
+                x_middle = x_least
+                f_middle = f_least
+
+                x_least = x_new
+                f_least = f_new
+
+            else:
+                if x_new < x_least:
+                    a = x_new
+                else:
+                    b = x_new
+
+                if f_new <= f_middle:
+                    x_largest = x_middle
+                    f_largest = f_middle
+
+                    x_middle = x_new
+                    f_middle = f_new
+
+                elif f_new <= f_largest:
+                    x_largest = x_new
+                    f_largest = f_new
+
         else:
-            if u >= x:
-                a = x
-            else:
-                b = x
-            v = w
-            w = x
-            x = u
-            fv = fw
-            fw = fx
-            fx = fu
+            print('Searching finished. Successfully. code 0')
+            return {'point': x_least, 'f_value': f_least}, history
+
+        if keep_history:
+            history = update_history(history, [i, f_least, f_middle, f_largest,
+                                               x_least, x_middle, x_largest, a, b, name_step])
+        if verbose:
+            print(f'iteration {i}\tx = {x_least:0.6f},\tf(x) = {f_least:0.6f}\ttype : {name_step}')
+
     else:
         print('Searching finished. Max iterations have been reached. code 1')
-        return x, fx, i
+        return {'point': x_least, 'f_value': f_least}, history
+
+
+def update_history(history: HistoryBrant, values: Sequence[Any]) -> HistoryBrant:
+    """
+    Updates brant history
+    :param history: HistoryBrant object in which the update is required
+    :param values: Sequence with values: 'iteration', 'f_least', 'f_middle', 'f_largest',  'x_least',
+                                         'x_middle', 'x_largest', 'left_bound', 'right_bound', 'type_step'
+    :return: updated HistoryBrant
+    """
+    name: Literal['iteration', 'f_least', 'f_middle', 'f_largest', 'x_least', 'x_middle',
+                  'x_largest', 'left_bound', 'right_bound', 'type_step']
+
+    for i, name in enumerate(['iteration', 'f_least', 'f_middle', 'f_largest', 'x_least', 'x_middle',
+                              'x_largest', 'left_bound', 'right_bound', 'type_step']):
+        history[name].append(values[i])
+
+    return history
 
 
 if __name__ == '__main__':
     def func(x): return x ** 3 - x ** 2 - x
     bs = [0, 1.5]
-    print(brant(func, bounds=bs, type_optimization='min'))
+    print(brant(func, bounds=bs, type_optimization='min', keep_history=True, verbose=True))
--- OneDimensionalOptimization/algorithms/support.py
+++ OneDimensionalOptimization/algorithms/support.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
-from typing import List, TypedDict
-from numbers import Real
+from typing import List, TypedDict, AnyStr
+from numbers import Real, Integral
 
 
 class HistoryGSS(TypedDict):
     """
     Class with an optimization history of GSS
     """
-    iteration: List[int]
+    iteration: List[Integral]
     middle_point: List[Real]
     f_value: List[Real]
     left_point: List[Real]
@@ -18,11 +18,31 @@ class HistorySPI(TypedDict):
     """
     Class with an optimization history of SPI
     """
-    iteration: List[int]
+    iteration: List[Integral]
     f_value: List[Real]
     x0: List[Real]
     x1: List[Real]
     x2: List[Real]
+
+
+class HistoryBrant(TypedDict):
+    """
+    Class with an optimization history of Brant's algorithm
+    """
+    iteration: List[Integral]
+
+    f_least: List[Real]
+    f_middle: List[Real]
+    f_largest: List[Real]
+
+    x_least: List[Real]
+    x_middle: List[Real]
+    x_largest: List[Real]
+
+    left_bound: List[Real]
+    right_bound: List[Real]
+
+    type_step: List[AnyStr]
 
 
 class Point(TypedDict):
