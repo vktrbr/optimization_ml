148c58b233ce242e160717c11cceb4397097134b
vktrbr <viktorbarbarich@gmail.com>
2022-03-22 23:54:00 +0300

    + bfgs alg. New page for bfgs + brent's visualization is not complete yet without button

--- OneDimensionalOptimization/streamlit_app.py
+++ OneDimensionalOptimization/streamlit_app.py
@@ -37,38 +37,77 @@ Available functions: $ \\
 \ \operatorname{acsc}(u), u!, \operatorname{sqrt}(u)$
 '''
 
-with st.sidebar.form('input_data'):
-    flag_empty_func = True
-    st.markdown('# Conditions:')
-    function = st.text_input('Enter the function here', 'x ** 3 - x ** 2 - x - log2(x + 2)',
-                             help=help_function_string)
+type_alg = st.sidebar.selectbox(r'Algorithm of optimization', algorithms_list)
 
-    if re.sub(r'\s', '', function) != '':
-        if '|' in function:
-            st.write('Use abs(h) instead of |h|')
-        else:
-            try:
-                function_latex = sympy.latex(sympy.sympify(function))
-                function_sympy = parse_func(function)
-                flag_empty_func = False
-            except (SyntaxError, TypeError, NameError):
-                st.write('Check syntax. Wrong input :(')
-            except (TokenError, SympifyError):
-                st.write('**Error**')
-                st.write('Write logarithms as: `log(x**2, x+1)` '
-                         'variables as `pi * x` instead of `pi x`')
+if type_alg in ["Golden-section search", "Successive parabolic interpolation", "Brent's method"]:
+    with st.sidebar.form('input_data'):
+        flag_empty_func = True
+        st.markdown('# Conditions:')
+        function = st.text_input('Enter the function here', 'x ** 3 - x ** 2 - x - log2(x + 2)',
+                                 help=help_function_string)
 
-        type_opt = st.radio('Optimization aim', ['min', 'max'])
+        if re.sub(r'\s', '', function) != '':
+            if '|' in function:
+                st.write('Use abs(h) instead of |h|')
+            else:
+                try:
+                    function_latex = sympy.latex(sympy.sympify(function))
+                    function_sympy = parse_func(function)
+                    flag_empty_func = False
+                except (SyntaxError, TypeError, NameError):
+                    st.write('Check syntax. Wrong input :(')
+                except (TokenError, SympifyError):
+                    st.write('**Error**')
+                    st.write('Write logarithms as: `log(x**2, x+1)` '
+                             'variables as `pi * x` instead of `pi x`')
 
-        bounds_a = st.number_input('Left bound', value=0., format='%.3f')
-        bounds_b = st.number_input('Right bound', value=2., format='%.3f')
-        bounds = sorted([bounds_a, bounds_b])
-        type_alg = st.selectbox('Algorithm of optimization', algorithms_list)
-        cnt_iterations = int(st.number_input('Maximum number of iterations', value=500, min_value=0), )
-        epsilon = float(st.number_input('epsilon', value=1e-5, min_value=1e-6, max_value=1., step=1e-6, format='%.6f'))
-        type_plot = st.radio('Type visualization', ['step-by-step', 'static'])
+            type_opt = st.radio('Optimization aim', ['min', 'max'])
+            bounds_a = st.number_input('Left bound', value=0., format='%.3f')
+            bounds_b = st.number_input('Right bound', value=2., format='%.3f')
+            bounds = sorted([bounds_a, bounds_b])
+            cnt_iterations = int(st.number_input('Maximum number of iterations', value=500, min_value=0), )
+            epsilon = float(st.number_input('epsilon', value=1e-6, min_value=1e-6,
+                                            max_value=1., step=1e-6, format='%.6f'))
+            if type_alg != "Brent's method":
+                type_plot = st.radio('Type visualization', ['step-by-step', 'static'])
+            else:
+                type_plot = 'static'
 
-    submit_button = st.form_submit_button(label='Solve!')
+        submit_button = st.form_submit_button(label='Solve!')
+
+elif type_alg == 'BFGS algorithm':
+    with st.sidebar.form('input_data'):
+        flag_empty_func = True
+        st.markdown('# Conditions:')
+        function = st.text_input('Enter the function here', 'x ** 3 - x ** 2 - x - log2(x + 2)',
+                                 help=help_function_string)
+
+        if re.sub(r'\s', '', function) != '':
+            if '|' in function:
+                st.write('Use abs(h) instead of |h|')
+            else:
+                try:
+                    function_latex = sympy.latex(sympy.sympify(function))
+                    function_sympy = parse_func(function)
+                    flag_empty_func = False
+                except (SyntaxError, TypeError, NameError):
+                    st.write('Check syntax. Wrong input :(')
+                except (TokenError, SympifyError):
+                    st.write('**Error**')
+                    st.write('Write logarithms as: `log(x**2, x+1)` '
+                             'variables as `pi * x` instead of `pi x`')
+            type_opt = 'min'
+            x0 = st.number_input('Start search point', value=2., format='%.3f')
+            cnt_iterations = int(st.number_input('Maximum number of iterations', value=100, min_value=0))
+            c1 = float(st.number_input('First Wolfe constant', value=1e-5,
+                                       min_value=1e-5, max_value=1., format='%.5f'))
+            c2 = float(st.number_input('Second Wolfe constant', value=0.9,
+                                       min_value=1e-2, max_value=1., format='%.2f'))
+            tolerance = float(
+                st.number_input('tolerance', value=1e-6, min_value=1e-6, max_value=1., step=1e-6, format='%.6f'))
+            type_plot = 'static'  # st.radio('Type visualization', ['step-by-step', 'static'])
+
+        submit_button = st.form_submit_button(label='Solve!')
 
 if not submit_button or flag_empty_func:
     title = st.title(r"1-D optimization")
@@ -83,8 +122,12 @@ if not submit_button or flag_empty_func:
 else:
     # Generate first raw
     st.write('### Problem:')
-    problem_latex = rf'$ \displaystyle f(x) = {function_latex}, \quad'
-    problem_latex += rf' x \in [{bounds_a: 0.3f}, {bounds_b :0.3f}] $'
+    if type_alg != 'BFGS algorithm':
+        problem_latex = rf'$ \displaystyle f(x) = {function_latex}, \quad'
+        problem_latex += rf' x \in [{bounds_a: 0.3f}, {bounds_b :0.3f}] $'
+    else:
+        problem_latex = rf'$ \displaystyle f(x) = {function_latex}, \quad'
+        problem_latex += rf' x_0 = {x0: 0.3f}$'
     st.write(problem_latex)
 
     # Generate second row
@@ -97,41 +140,52 @@ else:
     time_start = timeit.timeit()
     try:
         try:
-            point, history = solve_task(type_alg, function=function_callable, bounds=bounds, keep_history=True,
-                                        type_optimization=type_opt, max_iter=cnt_iterations, epsilon=epsilon)
+            if type_alg != 'BFGS algorithm':
+                point, history = solve_task(type_alg, function=function_callable, bounds=bounds, keep_history=True,
+                                            type_optimization=type_opt, max_iter=cnt_iterations, epsilon=epsilon)
+            else:
+                def function_callable_to_bfgs(x): return function_callable(x[0])
+                point, history = solve_task(type_alg, function=function_callable_to_bfgs, x0=x0,
+                                            keep_history=True, max_iter=cnt_iterations,
+                                            tolerance=tolerance, c1=c1, c2=c2)
+                diff_x = abs(x0 - point['point'])
+                bounds_a = min(x0, point['point']) - diff_x * 0.15
+                bounds_b = max(x0, point['point']) + diff_x * 0.15
+
+            total_time = timeit.timeit() - time_start
+
+            point_screen, f_value_screen, time_screen, iteration_screen = st.columns(4)
+            point_screen.write(r'$ x_{\ ' + f'{type_opt}' + '} = ' + f'{point["point"]: 0.4f} $')
+            f_value_screen.write(r'$ f_{\ ' + f'{type_opt}' + '} = ' + f'{point["f_value"]: 0.4f} $')
+            time_screen.write(f'**Time** = {abs(total_time): 0.6f} s.')
+            iteration_screen.write(f'**Iterations**: {len(history["iteration"])}')
+
+            if type_plot == 'static':
+                plotly_figure = gen_lineplot(function_callable,
+                                             [bounds_a, bounds_b],
+                                             [[point['point']], [point['f_value']]])
+
+            if len(history['iteration']) == 1:
+                st.write('**The solution found in 1 step!**')
+                plotly_figure = gen_lineplot(function_callable,
+                                             [bounds_a, bounds_b],
+                                             [[point['point']], [point['f_value']]])
+
+            elif type_plot == 'step-by-step' and type_alg == 'Golden-section search':
+                plotly_figure = gen_animation_gss(function_callable, bounds, history)
+
+            elif type_plot == 'step-by-step' and type_alg == 'Successive parabolic interpolation':
+                plotly_figure = gen_animation_spi(function_callable, bounds, history)
+
+            else:
+                plotly_figure = gen_lineplot(function_callable,
+                                             [bounds_a, bounds_b],
+                                             [[point['point']], [point['f_value']]])
+            figure = st.write(plotly_figure)
+
         except AssertionError:
             st.write('The method has diverged. Set new initial state.')
             st.stop()
-
-        total_time = timeit.timeit() - time_start
-
-        point_screen, f_value_screen, time_screen, iteration_screen = st.columns(4)
-        point_screen.write(r'$ x_{\ ' + f'{type_opt}' + '} = ' + f'{point["point"]: 0.4f} $')
-        f_value_screen.write(r'$ f_{\ ' + f'{type_opt}' + '} = ' + f'{point["f_value"]: 0.4f} $')
-        time_screen.write(f'**Time** = {abs(total_time): 0.6f} s.')
-        iteration_screen.write(f'**Iterations**: {len(history["iteration"])}')
-
-        if type_plot == 'static':
-            plotly_figure = gen_lineplot(function_callable,
-                                         [bounds_a, bounds_b],
-                                         [[point['point']], [point['f_value']]])
-        if len(history['iteration']) == 1:
-            st.write('**The solution found in 1 step!**')
-            plotly_figure = gen_lineplot(function_callable,
-                                         [bounds_a, bounds_b],
-                                         [[point['point']], [point['f_value']]])
-
-        elif type_plot == 'step-by-step' and type_alg == 'Golden-section search':
-            plotly_figure = gen_animation_gss(function_callable, bounds, history)
-
-        elif type_plot == 'step-by-step' and type_alg == 'Successive parabolic interpolation':
-            plotly_figure = gen_animation_spi(function_callable, bounds, history)
-
-        else:
-            plotly_figure = gen_lineplot(function_callable,
-                                         [bounds_a, bounds_b],
-                                         [[point['point']], [point['f_value']]])
-        figure = st.write(plotly_figure)
     except NameError:
         st.write('Check syntax. Wrong input :(')
         st.stop()
