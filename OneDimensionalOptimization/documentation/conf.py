913f005afe8d18e84c995ad3c50cc96a2ac3bbf1
vktrbr <viktorbarbarich@gmail.com>
2022-03-22 21:50:55 +0300

    added latex_elements

--- OneDimensionalOptimization/documentation/conf.py
+++ OneDimensionalOptimization/documentation/conf.py
@@ -3,9 +3,7 @@ import sys
 import pathlib
 sys.path.insert(0, str(pathlib.Path(os.path.abspath('.')).parents[1]))
 
-import OneDimensionalOptimization
-
-project = 'One-Dimensional-Optimization'
+project = 'One Dimensional Optimization'
 copyright = '2022, Victor Barbarich'
 author = 'Victor Barbarich'
 release = '1.0.0'
@@ -18,3 +16,4 @@ add_module_names = False
 html_static_path = ['_static']
 autodoc_typehints = "description"
 autodoc_class_signature = "separated"
+latex_elements = {'extraclassoptions': 'openany,oneside'}
