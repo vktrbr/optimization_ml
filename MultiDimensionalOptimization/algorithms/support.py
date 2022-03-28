30e3de59cf39eb5458e08927dad5b9b1f947084a
vktrbr <viktorbarbarich@gmail.com>
2022-03-29 01:32:16 +0300

    + new interesting HiddenPrints class updated gradient with numpy and dtypes

--- MultiDimensionalOptimization/algorithms/support.py
+++ MultiDimensionalOptimization/algorithms/support.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
-from typing import List, TypedDict, AnyStr, Literal
+from typing import List, TypedDict, AnyStr, Literal, Callable, Sequence
 from numbers import Real, Integral
+import numpy as np
+import os
+import sys
 
 
 class Point(TypedDict):
@@ -18,8 +21,24 @@ class HistoryGradDescent(TypedDict):
     iteration: List[Integral]
     f_value: List[Real]
     f_grad_norm: List[Real]
-    x: List[Real]
+    x: List[Sequence]
     message: AnyStr
+
+
+class HiddenPrints:
+    """
+    Object hides print. Working with context manager "with"::
+
+        >>> with HiddenPrints():
+        >>>     print("It won't be printed")
+    """
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
 
 
 def update_history_grad_descent(history: HistoryGradDescent, values: List) -> HistoryGradDescent:
@@ -31,5 +50,31 @@ def update_history_grad_descent(history: HistoryGradDescent, values: List) -> Hi
     """
     key: Literal['iteration', 'f_value', 'f_grad_norm', 'x']
     for i, key in enumerate(['iteration', 'f_value', 'f_grad_norm', 'x']):
-        history[key] = values[i]
+        history[key].append(values[i])
     return history
+
+
+def gradient(function: Callable,
+             x0: np.ndarray,
+             delta_x=1e-8) -> np.ndarray:
+    """
+    Calculate and return a gradient using a two-side difference
+    :param function: callable that depends on the first positional argument
+    :param x0: the point at which we calculate the gradient
+    :param delta_x: precision of differentiation
+    :return: vector np.ndarray with the gradient at the point
+    """
+
+    grad = []
+    x0 = np.array(x0, dtype=float)
+
+    for i in range(len(x0)):
+        delta = np.zeros_like(x0)
+        delta[i] += delta_x
+        delta_x_vec_plus = x0 + delta
+        delta_x_vec_minus = x0 - delta
+        grad_i = (function(delta_x_vec_plus) - function(delta_x_vec_minus)) / (2 * delta_x)
+        grad.append(grad_i)
+
+    grad = np.array(grad)
+    return grad
