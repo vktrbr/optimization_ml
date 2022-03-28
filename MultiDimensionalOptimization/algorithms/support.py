93506f3089ba19ffb792980f890a6fccab484cf2
vktrbr <viktorbarbarich@gmail.com>
2022-03-28 17:45:18 +0300

    history will same for each grad method created update_history_grad_descent which update history

--- MultiDimensionalOptimization/algorithms/support.py
+++ MultiDimensionalOptimization/algorithms/support.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import List, TypedDict, AnyStr, Tuple
+from typing import List, TypedDict, AnyStr, Literal
 from numbers import Real, Integral
 
 
@@ -11,11 +11,25 @@ class Point(TypedDict):
     f_value: Real
 
 
-class HistoryGDFS(TypedDict):
+class HistoryGradDescent(TypedDict):
     """
-    Class with an optimization history of GDFS
-    (gradient descent with fractal step)
+    Class with an optimization history of gradient descent methods
     """
     iteration: List[Integral]
     f_value: List[Real]
+    f_grad_norm: List[Real]
     x: List[Real]
+    message: AnyStr
+
+
+def update_history_grad_descent(history: HistoryGradDescent, values: List) -> HistoryGradDescent:
+    """
+    Update HistoryGradDescent with values, which contains iteration, f_value, f_grad_norm, x as a list
+    :param history: object of HistoryGradDescent
+    :param values: new values that need to append in history in order iteration, f_value, f_grad_norm, x
+    :return: updated history
+    """
+    key: Literal['iteration', 'f_value', 'f_grad_norm', 'x']
+    for i, key in enumerate(['iteration', 'f_value', 'f_grad_norm', 'x']):
+        history[key] = values[i]
+    return history
