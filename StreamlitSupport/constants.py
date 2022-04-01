fbea64232669517686a7bdedc115335fd3055e16
vktrbr <viktorbarbarich@gmail.com>
2022-04-02 00:34:18 +0300

    constants.py will updated

---
+++ StreamlitSupport/constants.py
@@ -0,0 +1,10 @@
+help_function_string = r'''
+Available functions: $ \\ 
+\log(u, v) \ (v - \text{base}), \ \exp(u),\ \operatorname{abs}(u), \ \tan(u), \ \cot(u), \\
+\sin(u), \ \cos(u), \ \operatorname{asin}(u), \ \operatorname{sec}(u) \ \operatorname{csc}(u),
+\ \operatorname{sinc}(u), \\ \ \operatorname{asin}(u), \ \operatorname{acos}(u), 
+\ \operatorname{atan}(u), \ \operatorname{acot}(u), \ \operatorname{asec}(u), 
+\ \operatorname{acsc}(u), u!, \operatorname{sqrt}(u)$
+'''
+
+
