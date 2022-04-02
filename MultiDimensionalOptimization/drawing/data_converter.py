3c4c00355dd15a9c41f3247f50a62a87eaa7d170
vktrbr <viktorbarbarich@gmail.com>
2022-04-02 14:45:15 +0300

    increased drawing intervals

--- MultiDimensionalOptimization/drawing/data_converter.py
+++ MultiDimensionalOptimization/drawing/data_converter.py
@@ -105,7 +105,7 @@ def make_ranges(history: HistoryMDO) -> Tuple[List, List]:
     min_y = descent_history.y.min()
     max_y = descent_history.y.max()
 
-    x_range = [min_x - (max_x - min_x) * 0.1, max_x + (max_x - min_x) * 0.1]
-    y_range = [min_y - (max_y - min_y) * 0.1, max_y + (max_y - min_y) * 0.1]
+    x_range = [min_x - (max_x - min_x) * 0.7, max_x + (max_x - min_x) * 0.7]
+    y_range = [min_y - (max_y - min_y) * 0.7, max_y + (max_y - min_y) * 0.7]
 
     return x_range, y_range
