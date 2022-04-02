fde98a2d173e4724d3b60eb527ff2775f85c0ce2
vktrbr <viktorbarbarich@gmail.com>
2022-04-02 15:28:33 +0300

    sorted vars

--- MultiDimensionalOptimization/parser/sympy_parser.py
+++ MultiDimensionalOptimization/parser/sympy_parser.py
@@ -8,16 +8,18 @@ def sympy_to_callable(function_sympy: sympy.core.expr.Expr) -> Tuple[Callable, I
     """
     Convert sympy expression to callable function, for example::
 
-        >>> f = parse_func('x**2')
+        >>> f = parse_func('x ** 2 + y ** 2')
         >>> f, _ = sympy_to_callable(f)
         >>> f
         <function_lambdifygenerated(x)>
-        >>> f(2)
-        4
+        >>> f(2, 2)
+        8
     :param function_sympy: sympy expression
     :except: AssertionError. If function depends on more one variable
-    :return: callable function from one argument and number of variables
+    :return: callable function from number of varialbes arguments and number of variables
 
     """
     var = list(function_sympy.free_symbols)
-    return sympy.lambdify(var, function_sympy), len(var)
+    var = sorted(var, key=lambda x: str(x))
+
+    return sympy.lambdify(var, function_sympy), var
