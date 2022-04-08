9acdf855cf00bf1c504c1181c02a1951f322a3e6
adelinatsoi <dlnxxts@gmail.com>
2022-04-08 15:38:01 +0300

    add None button for regulators, PEP changes

--- Regression/streamlit_app.py
+++ Regression/streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.set_page_config(
     page_title=r"Regression",
     page_icon=":four:", )  # make page name
-column_names = ['x1', 'x2', 'y']  #name of GIVEN columns
+column_names = ['x1', 'x2', 'y']  # name of GIVEN columns
 regression_types = ['Linear', 'Polynomial', 'Exponential']
-regulators_types = ['L1', 'Tikhonov (L2)']
+regulators_types = ['L1', 'Tikhonov (L2)', 'None']
 opt_alg = ['By matrices', 'By gradient methods']
 data = st.write('data')  # place for getting excel file
 
@@ -17,10 +17,8 @@ with st.sidebar.form('input_data'):
 
     if regression_type == 'Polynomial':
         degree = int(st.number_input('degree of polynomial regression', value=1, min_value=1,
-                        max_value=10, step=1))
+                     max_value=10, step=1))
     regulators_type = st.selectbox(r'Regulators type', regulators_types)
     optim_alg = st.selectbox(r'Optimization algorithm', opt_alg)
 
     submit_button = st.form_submit_button(label='Solve!')
-
-
