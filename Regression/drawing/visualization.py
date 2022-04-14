7d79013a6545eda077c333a9b6c490cad40c5bd5
adelinatsoi <dlnxxts@gmail.com>
2022-04-15 01:38:27 +0300

    polyplot and exponential plots

--- Regression/drawing/visualization.py
+++ Regression/drawing/visualization.py
@@ -24,3 +24,31 @@ def gen_polyplot(x: np.ndarray, y: np.ndarray, w: np.ndarray) -> go.Figure:
                       yaxis_title=r'<b>y</b>',
                       font=dict(size=14))
     return fig
+
+
+def gen_exponential_plot(x: np.ndarray, y: np.ndarray, w: np.ndarray) -> go.Figure:
+    """
+    y = w[0] * exp(X @ w[2:]) + w[1]
+
+    :param x:
+    :param y:
+    :param w:
+    :return:
+    """
+    x = x.astype(np.float)
+    y = y.astype(np.float)
+    w = w.astype(np.float)
+
+
+    min_max_delta = 0.01 * (np.max(x) - np.min(x))
+    x_axis = np.linspace(np.min(x) - min_max_delta, np.max(x) + min_max_delta, 50)
+    y_axis = w[0] * np.exp(x_axis.reshape(-1, 1) @ w[2:].reshape(-1, 1)) + w[1]
+    fig = px.scatter({'x': x.reshape(-1), 'initial distr': y}, x='x', y=['initial distr'],
+                     title='<b>Regression plot</b>')
+
+    fig.add_trace(go.Scatter(x=x_axis, y=y_axis.reshape(-1), mode='lines', name='regression'))
+    fig.update_layout(xaxis_title=r'<b>x</b>',
+                      yaxis_title=r'<b>y</b>',
+                      font=dict(size=14))
+
+    return fig
