2d2c51b6088f5faed7875bcc5feabc5f17db43dd
vktrbr <viktorbarbarich@gmail.com>
2022-05-17 01:17:35 +0300

    empty streamlit

--- Classification/streamlit_app.py
+++
@@ -1,6 +0,0 @@
-import torch
-import streamlit as st
-
-# st.write(torch.FloatTensor([1., 2.]))
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using device:', device)
